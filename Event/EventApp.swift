07a779c95cf7e9fbefcf7a87ecb843ae7d80d706
Virgiel <>
2022-12-12 14:06:04 +0100

    Init

---
+++ Event/EventApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventApp.swift
+//  Event
+//
+//  Created by antoine on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct EventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
