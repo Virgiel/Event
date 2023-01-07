9500e3dd7e3882dbe952c5c4b8b746f7db1b9aa3
Virgiel <>
2023-01-07 13:22:40 +0100

    Improve carousel

--- Event/CarrouselView.swift
+++ Event/CarrouselView.swift
@@ -6,6 +6,9 @@ import SwiftUI
 
 struct Day: View {
     var body: some View {
+        List {
+            
+        }
         VStack {
             ForEach(0 ..< 24) { index in
                 Text("\(index):00").frame(maxWidth: .infinity, minHeight: 100)
@@ -14,59 +17,94 @@ struct Day: View {
     }
 }
 
-struct Carousel: View {
+class CarouselState: ObservableObject {
+    let count: Int
+    @Published private(set) var selected: Int;
+    
+    init(count: Int, selected: Int = 0) {
+        assert(count >= 0 && selected >= 0 && selected < count)
+        self.count = count;
+        self.selected = selected;
+    }
+
+    
+    func has_next() -> Bool {
+        selected < count-1
+    }
+    
+    func has_prev() -> Bool {
+        selected > 0
+    }
+    
+    func next() {
+        if has_next() {
+            selected += 1;
+        }
+    }
+    
+    func prev() {
+        if has_prev() {
+            selected -= 1;
+        }
+    }
+}
+
+struct Carousel<Content: View>: View {
     @GestureState private var isDetectingLongPress = false
     @State private var dragOff: CGFloat? = nil
-    @Binding var selected: Int
-    private let count: Int = 3
+    @ObservedObject var state: CarouselState
+    @ViewBuilder var content: (Int) -> Content
 
     var body: some View {
         GeometryReader {
             geometry in
             ScrollView {
                 HStack(alignment: .center, spacing: .zero) {
-                    ForEach([Color.red, Color.blue,  Color.yellow], id: \.self) { item in
-                        Day().frame(width: geometry.size.width)
-                                .background(item)
+                    ForEach(0..<state.count, id: \.self) { i in
+                        VStack {
+                            content(i)
+                        }.frame(width: geometry.size.width)
                     }
                 }
             }
-                    .offset(x: (-CGFloat(selected) * (geometry.size.width)) + (dragOff ?? 0))
-                    .animation(.easeInOut(duration: 0.3), value: dragOff == nil)
-                    .animation(.easeInOut(duration: 0.3), value: selected)
-                    .gesture(
-                            DragGesture()
-                                    .onChanged { value in
-                                        dragOff = value.translation.width
-                                    }
-                                    .onEnded { value in
-                                        let threeshold = geometry.size.width / 6
-                                        if value.translation.width < -threeshold && selected < count {
-                                            selected += 1
-                                        } else if value.translation.width > threeshold && selected > 0 {
-                                            selected -= 1
-                                        }
-                                        dragOff = nil
-                                    }
-                    )
+            .offset(x: (-CGFloat(state.selected) * (geometry.size.width)) + (dragOff ?? 0))
+            .animation(.easeInOut(duration: 0.3), value: dragOff == nil)
+            .animation(.easeInOut(duration: 0.3), value: state.selected)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        dragOff = value.translation.width
+                    }
+                    .onEnded { value in
+                        let threeshold = geometry.size.width / 6
+                        if value.translation.width < -threeshold {
+                            state.next()
+                        } else if value.translation.width > threeshold {
+                            state.prev()
+                        }
+                        dragOff = nil
+                    }
+            )
         }
     }
 }
 
+let color = [Color.red, Color.blue,  Color.yellow];
 
 struct CarouselExample: View {
-    @State private var selected: Int = 0
+    @StateObject private var state = CarouselState(count: color.count)
     var body: some View {
         VStack(alignment: .center)  {
-            Carousel(selected: $selected)
             HStack {
-                Button(action: {selected -= 1}) {
+                Button(action: {state.prev()}) {
                     Text("prev")
-                }.disabled(selected == 0)
-                Button(action: {selected += 1}) {
+                }.disabled(!state.has_prev())
+                Text("Jour \(state.selected)")
+                Button(action: {state.next()}) {
                     Text("next")
-                }.disabled(selected == 2)
+                }.disabled(!state.has_next())
             }
+            Carousel(state: state, content: {(i) in Day().background(color[i]) })
         }
     }
 }
